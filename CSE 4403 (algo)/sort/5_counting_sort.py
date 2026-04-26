5c1e004c950458e705d938b567551667a624d343
Mohammad Faiaj Jarif <u2203012@student.cuet.ac.bd>
2026-04-26 18:02:40 +0600

    4th commit

--- CSE 4403 (algo)/sort/5_counting_sort.py
+++ CSE 4403 (algo)/sort/5_counting_sort.py
@@ -1,1 +1,41 @@
 import comparator
+
+
+def counting_sort(arr):
+
+    min_val = min(arr)
+    max_val = max(arr)
+
+    # Count frequency
+    count = [0] * (max_val - min_val + 1)
+    for num in arr:
+        count[num - min_val] += 1
+
+    # converting count to prefix_sum
+    prefix = count
+    for i in range(1, len(prefix)):
+        prefix[i] += prefix[i - 1]
+
+    # converting prefix to index
+    index = prefix
+    for i in range (len(index) - 1, 0, -1):
+        index[i] = index[i - 1]
+
+    index[0] = 0
+    ans = [None] * len(arr)
+
+    for num in arr:
+        ans[index[num - min_val]] = num
+        index[num - min_val] += 1
+
+    print(ans)
+
+
+def main():
+    a = [1, -2, 5, 0, -3]
+    a = counting_sort(a)
+    # print(a)
+
+
+if __name__ == "__main__":
+    main()
