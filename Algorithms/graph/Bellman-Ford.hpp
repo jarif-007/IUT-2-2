0e6f84ae55b6364f1d5a77c484e2411ff65af6db
Mohammad Faiaj Jarif <u2203012@student.cuet.ac.bd>
2026-05-19 14:25:31 +0600

    bellman-ford fix

--- Algorithms/graph/Bellman-Ford.hpp
+++ Algorithms/graph/Bellman-Ford.hpp
@@ -7,16 +7,18 @@ vector<int> bellman_ford(vector<tuple<int, int, int>> &edges, int source)
     for (auto &[u, v, wt] : edges)
         maxNode = max(maxNode, max(u, v));
 
-    vector<int> dist(maxNode + 1, INT_MAX);
+
+    int V = maxNode + 1;
+    vector<int> dist(V, INT_MAX);
     dist[source] = 0;
 
-    for (int i = 0; i < maxNode; i++)
+    for (int i = 0; i < V; i++)
     {
         for (auto &[u, v, wt] : edges)
         {
             if (dist[u] != INT_MAX && dist[u] + wt < dist[v])
             {
-                if (i == v - 1)
+                if (i == V - 1)
                     return {-1};
 
                 dist[v] = dist[u] + wt;
